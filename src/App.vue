4aecceb8e18c86caf9f7245a2747ffbfaa1a30b3
Kolya Movchan <kmovchan261@gmail.com>
2023-07-18 11:14:12 +0300

    Set Up environment

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="app">
+    <Logo />
+    <RouterView />
+  </div>
+</template>
+
+<script setup>
+  import { RouterView } from "vue-router";
+  import Logo from "./components/Logo.vue";
+</script>
+
+<style lang="scss" scoped>
+
+.app {
+  height: 100vh;
+  background: linear-gradient(to top, #fff1eb 0%, #ace0f9 100%);
+}
+
+</style>
