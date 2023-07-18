9630ba92d9cb71db8bf83fefa6073db87984d333
Kolya Movchan <kmovchan261@gmail.com>
2023-07-18 15:17:18 +0300

    Check predeploy

--- deploy.sh
+++ deploy.sh
@@ -9,10 +9,14 @@ npm run build
 # navigate into the build output directory
 cd dist
 
+# place .nojekyll to bypass Jekyll processing
+echo > .nojekyll
+
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
 
 git init
+git checkout -B main
 git add -A
 git commit -m 'deploy'
 
