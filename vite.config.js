2861cf6909c27ab0ee8a067d86daf0b01b2c00d3
Kolya Movchan <kmovchan261@gmail.com>
2023-07-21 15:14:55 +0300

    predeploy

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,8 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  publicPath: '/weather-app/',
-  plugins: [
-    vue(),
-  ],
+  base: '/weather-app/',
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
