063b621bdb9eedd3e694beee1a5327e5916f9055
Mykola Movchan <104283893+kolya-movchan@users.noreply.github.com>
2023-07-21 17:11:08 +0300

    Create deploy2.sh

---
+++ .github/workflows/deploy2.sh
@@ -0,0 +1,19 @@
+
+# abort on errors
+set -e
+
+# build
+npm run build
+
+cd dist
+
+echo > .nojekyll
+
+git init
+git checkout -B main
+git add -A
+git commit -m 'deploy'
+
+git push -f https://github.com/kolya-movchan/weather-app.git main:gh-pages
+
+cd -
