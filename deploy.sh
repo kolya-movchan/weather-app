03892143c1deb3a91c8abc9eaf657edf2a86e205
Kolya Movchan <kmovchan261@gmail.com>
2023-07-21 15:53:57 +0300

    deploy

--- deploy.sh
+++ deploy.sh
@@ -20,11 +20,7 @@ git checkout -B main
 git add -A
 git commit -m 'deploy'
 
-# if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
-
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
 git push -f git@github.com:kolya-movchan/weather-app.git main:gh-pages
 
 cd -
